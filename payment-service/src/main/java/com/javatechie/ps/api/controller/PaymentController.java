2532cc4ddac4a32d85e386acd8c2dc6262fe40c1
Ayman ZAABAR <ayman.zaabar@proxym-it.com>
2023-02-14 16:52:42 +0100

    cloud config server done

--- payment-service/src/main/java/com/javatechie/ps/api/controller/PaymentController.java
+++ payment-service/src/main/java/com/javatechie/ps/api/controller/PaymentController.java
@@ -1,12 +1,10 @@
 package com.javatechie.ps.api.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.javatechie.ps.api.entity.Payment;
 import com.javatechie.ps.api.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Random;
 
@@ -18,10 +16,13 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping("/doPayment")
-    public Payment doPayment(@RequestBody Payment payment){
+    public Payment doPayment(@RequestBody Payment payment) throws JsonProcessingException {
         return paymentService.doPayment(payment);
     }
 
-
+    @GetMapping("/{orderId}")
+    public Payment findPaymentHistoryByOrderId(@PathVariable int orderId) throws JsonProcessingException {
+        return paymentService.findPaymentHistoryByOrderId(orderId);
+    }
 
 }
--- payment-service/src/main/java/com/javatechie/ps/api/service/PaymentService.java
+++ payment-service/src/main/java/com/javatechie/ps/api/service/PaymentService.java
@@ -1,7 +1,11 @@
 package com.javatechie.ps.api.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.javatechie.ps.api.entity.Payment;
 import com.javatechie.ps.api.repository.PaymentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,17 +14,29 @@ import java.util.UUID;
 
 @Service
 public class PaymentService {
-@Autowired
-    private PaymentRepository paymentRepository;
+    @Autowired
+    private PaymentRepository repository;
 
-public Payment doPayment (Payment payment){
-    payment.setPaymentStatus(paymentProcessing());
-    payment.setTransactionId(UUID.randomUUID().toString());
-    return paymentRepository.save(payment);
-}
+    Logger logger= LoggerFactory.getLogger(PaymentService.class);
+
+    public Payment doPayment(Payment payment) throws JsonProcessingException {
+        payment.setPaymentStatus(paymentProcessing());
+        payment.setTransactionId(UUID.randomUUID().toString());
+        logger.info("Payment-Service Request : {}",new ObjectMapper().writeValueAsString(payment));
+
+        return repository.save(payment);
+    }
+
 
     public String paymentProcessing(){
-    //api should be 3rd party payment gateway(paypal,paytm...)
+        //api should be 3rd party payment gateway (paypal,paytm...)
         return new Random().nextBoolean()?"success":"false";
+    }
+
+
+    public Payment findPaymentHistoryByOrderId(int orderId) throws JsonProcessingException {
+        Payment payment=repository.findByOrderId(orderId);
+        logger.info("paymentService findPaymentHistoryByOrderId : {}",new ObjectMapper().writeValueAsString(payment));
+        return payment ;
     }
 }
